d4bc8df3c3637201515200b8773cc18d7011be6f
Oviedo404 <oviedovaleria@comunidad.unam.mx>
2020-06-07 21:23:38 -0500

    FEAT:Se añadió la carpeta Actividad-8

---
+++ Actividad-8/dynamics/PHP/cifradoC.php
@@ -0,0 +1,42 @@
+<?php
+echo "<!DOCTYPE html>
+  <html lang='es' dir='ltr'>
+  <head>
+    <meta charset='utf-8'>
+    <title>Profesor/funcionario</title>
+    <style>
+      body{
+        background-color:rgb(123, 208, 129);
+        width: 50%;
+        margin: 0 auto;
+        text-align: center;
+        font-size: 18px;
+      }
+      h2{
+        color:rgb(28, 62, 44);
+      }
+    </style>
+  </head>
+  <body>";
+    $texto=(isset($_POST['texto']) && !is_numeric($_POST['texto']))? $_POST['texto']:'0';
+    if($texto == '0'){
+      header('Location:/templates/cifradoC.html');
+    }else{
+      $uno=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'
+      ,'W','X','Y','Z','.',',','?','¿'];
+      $dos=['L','M','N','O','P','Q','R','S','T','U','V'
+      ,'W','X','Y','Z','.',',','?','¿','A','B','C','D','E','F','G','H','I','J','K'];
+      $esp=['Á','É','Í','Ó','Ú','Ñ'];
+      $texto=mb_strtoupper($texto);
+      $textoEsp= preg_match_all('/[ÁÉÍÓÚÑ]/',$texto);
+      if($textoEsp==true){
+          header('Location:/templates/cifradoC.html');
+      }else{
+        $textoCif=str_replace($uno, $dos, $texto);
+        echo "<br><h2>Texto cifrado: ".$textoCif."</h2>";
+      }
+    }
+    echo "</br></br><a href='../../../templates/cifradoC.html'>Regresar</a>";
+echo "</body>
+    </html>";
+?>
